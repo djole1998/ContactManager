b7616a5f70be235a98818eb70d98c1220dc3c4ea
Djole <djole98sp@live.com>
2018-09-16 16:42:55 +0200

    init commit

---
+++ src/components/test/Test.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+    // componentDidMount() {
+    //
+    // }
+
+    render() {
+        return (
+            <div>
+                <h1>Test Component</h1>
+            </div>
+        )
+    }
+}
+
+export default Test;
